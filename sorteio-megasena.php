6bb77b61908817cd79f30e6aa4e8dbef2b65c977
Alex Boccia <alexboccia@MacBook-Pro-de-Alex.local>
2019-10-24 20:37:24 -0300

    publicar arquivo no repositório

---
+++ sorteio-megasena.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Sorteio Megasena</title>
+</head>
+<body>
+
+	<h1><b>SORTEIO DA MEGASENA</b></h1>
+
+	<hr>
+
+	<br>
+
+	<form method="post">
+		<fieldset>
+			<legend><b>Informe à quantidade de dezenas</b></legend>
+			<input type="number" name="dezenas">
+			<input type="submit" name="Enviar">
+		</fieldset>
+	</form>
+
+	<?php
+		$dezenas = $_POST["dezenas"];
+
+		if("$dezenas" <= 0){
+			echo "<h3 style='color:red;>Informe um valor acima de 0.</h3>";
+		}
+
+		echo "<br><table border=0 style='width:100%'>";
+		echo "<tr>";
+
+		for($i=0; $i<=$dezenas; $i++){			
+			echo "<th style='background-color:#F0F8FF'>";
+			echo "<h1>" . mt_rand(0, 90) . "</h1>";
+			echo "</th>";
+		}
+
+		echo "</tr>";
+		echo "</table>";
+
+	?>
+</body>
+</html>
